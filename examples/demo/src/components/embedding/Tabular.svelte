53a76370b28553a44249247c56e3778c43dd1b49
Jay Wang <jay@zijie.wang>
2023-01-30 22:23:55 -0500

    Add demo skeleton
    Signed-off-by: Jay Wang <jay@zijie.wang>

---
+++ examples/demo/src/components/embedding/Tabular.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import iconGear from '../../imgs/icon-gear.svg?raw';
+
+  let component: HTMLElement | null = null;
+  let mounted = false;
+  let initialized = false;
+
+  onMount(() => {
+    mounted = true;
+  });
+
+  /**
+   * Initialize the embedding view.
+   */
+  const initView = () => {
+    initialized = true;
+
+    if (component) {
+      // Pass
+    }
+  };
+
+  $: mounted && !initialized && component && initView();
+</script>
+
+<style lang="scss">
+  @import './Tabular.scss';
+</style>
+
+<div class="tabular-wrapper" bind:this={component}>
+  <div class="tabular" />
+</div>
---
+++ examples/demo/src/components/webshap/WebShap.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+  import iconGithub from '../../imgs/icon-github.svg?raw';
+
+  let component: HTMLElement | null = null;
+  let view = 'loan-prediction';
+
+  // Initialize the stores to pass to child components
+  // const tooltipStore = getTooltipStore();
+</script>
+
+<style lang="scss">
+  @import './WebShap.scss';
+</style>
+
+<div class="diffusiondbvis-page">
+  <!-- <Tooltip {tooltipStore} /> -->
+
+  <div class="app-wrapper">
+    <div class="app-title">
+      <div class="title-left">
+        <!-- <div class="app-icon">
+          {@html logoDiffusionDB}
+        </div> -->
+        <span class="app-name"> WebSHAP Demo </span>
+      </div>
+
+      <div class="title-right">
+        <a class="title-link" href="https://github.com/anonacl/diffusiondb">
+          {@html iconGithub}
+        </a>
+      </div>
+    </div>
+
+    <div class="main-app" bind:this={component}>
+      <div
+        class="main-app-container"
+        class:hidden={view !== 'loan-prediction'}
+      />
+
+      <div
+        class="main-app-container"
+        class:hidden={view !== 'image-classification'}
+      />
+
+      <div
+        class="main-app-container"
+        class:hidden={view !== 'text-classification'}
+      />
+    </div>
+
+    <div class="app-tabs">
+      <button
+        class="tab"
+        class:selected={view === 'loan-prediction'}
+        on:click={() => {
+          view = 'loan-prediction';
+        }}
+        data-text="Loan Approval Prediction">Loan Approval Prediction</button
+      >
+
+      <span class="splitter" />
+
+      <button
+        class="tab"
+        class:selected={view === 'image-classification'}
+        on:click={() => {
+          view = 'image-classification';
+        }}
+        data-text="Image Classification">Image Classification</button
+      >
+
+      <span class="splitter" />
+
+      <button
+        class="tab"
+        class:selected={view === 'text-classification'}
+        on:click={() => {
+          view = 'text-classification';
+        }}
+        data-text="Text Classification">Text Classification</button
+      >
+    </div>
+  </div>
+</div>
