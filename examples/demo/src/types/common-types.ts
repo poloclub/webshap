bc39af23c23792c8c8574af54133c15ae114943b
Jay Wang <jay@zijie.wang>
2023-03-11 19:40:00 -0500

    Add function to mask image
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- examples/demo/src/types/common-types.ts
+++ examples/demo/src/types/common-types.ts
@@ -6,6 +6,12 @@ import type { Tensor3D } from '@tensorflow/tfjs';
 
 type FeatureType = 'cont' | 'cat';
 
+export interface ImageSegmentation {
+  segData: ImageData;
+  segRGBData: ImageData;
+  segSize: number;
+}
+
 export interface LoadedImage {
   imageData: ImageData;
   imageTensor: Tensor3D;
