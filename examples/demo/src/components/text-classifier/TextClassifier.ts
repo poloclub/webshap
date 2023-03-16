4158c9c1f9e464ed4fd1f2582b7b3406c04d3d78
Jay Wang <xiao.hk1997@gmail.com>
2023-03-16 17:52:13 -0400

    Revert "Add text block placeholder"
    This reverts commit 1935fa1165cc68173b2a547b725e08cbd26e3470.

--- examples/demo/src/components/text-classifier/TextClassifier.ts
+++ examples/demo/src/components/text-classifier/TextClassifier.ts
@@ -10,7 +10,7 @@ import type {
   SHAPRow
 } from '../../types/common-types';
 import { KernelSHAP } from 'webshap';
-import { round, timeit, downloadJSON, haveContrast } from '../../utils/utils';
+import { round, timeit, downloadJSON } from '../../utils/utils';
 import { getLatoTextWidth } from '../../utils/text-width';
 
 const DEBUG = config.debug;
@@ -26,7 +26,6 @@ const RANDOM_UNIFORM = d3.randomUniform.source(LCG);
 export class TextClassifier {
   component: HTMLElement;
   textClassifierUpdated: () => void;
-  inputText: string;
 
   // Visualization
   colorScale: (t: number) => string;
@@ -53,7 +52,6 @@ export class TextClassifier {
   }) {
     this.component = component;
     this.textClassifierUpdated = textClassifierUpdated;
-    this.inputText = defaultInput;
 
     // Initialize SVG elements
     this.colorScale = d3.piecewise(d3.interpolateHsl, [
@@ -116,7 +114,7 @@ export class TextClassifier {
     const gradients = this.colorScaleSVG
       .append('defs')
       .append('linearGradient')
-      .attr('gradientTransform', 'rotate(-90 0.5 0.5)')
+      .attr('gradientTransform', 'rotate(90)')
       .attr('id', 'scale-gradient-text');
 
     const splits = 5;
@@ -133,8 +131,8 @@ export class TextClassifier {
 
     // Add a legend on the left of the color scale
     this.shapLengthScale.range([
-      svgSize.height - svgPadding.top - svgPadding.bottom - 2,
-      0
+      0,
+      svgSize.height - svgPadding.top - svgPadding.bottom - 2
     ]);
 
     this.colorLegendAxis = d3
@@ -147,182 +145,5 @@ export class TextClassifier {
       .tickFormat(d3.format('.2f'));
     axisGroup.call(this.colorLegendAxis);
     axisGroup.attr('font-size', null);
-  };
-
-  updateTextBlock = () => {
-    let words = [];
-    let curWord = '';
-
-    // eslint-disable-next-line quotes
-    const punctuations = new Set([',', '.', '/', '?', '!', '@', "'", '"', '`']);
-
-    for (let i = 0; i < this.inputText.length; i++) {
-      const curChar = this.inputText[i];
-      if (curChar === ' ') {
-        if (curWord !== '') {
-          words.push(curWord);
-        }
-        curWord = '';
-        // eslint-disable-next-line quotes
-      } else if (curChar === "'") {
-        if (curWord !== '') {
-          words.push(curWord);
-          curWord = '';
-        }
-        // eslint-disable-next-line quotes
-        curWord = "'";
-      } else if (punctuations.has(curChar)) {
-        if (curWord !== '') {
-          words.push(curWord);
-          words.push(curChar);
-          curWord = '';
-        } else {
-          words.push(curChar);
-        }
-      } else {
-        curWord += curChar.toLowerCase();
-      }
-    }
-
-    words = [
-      'son',
-      ', ',
-      'you',
-      "'",
-      're ',
-      'too ',
-      'young ',
-      'and ',
-      'stupid ',
-      'to ',
-      'tell ',
-      'me ',
-      'that ',
-      'you ',
-      'know ',
-      'enough ',
-      'to ',
-      'claim ',
-      'what ',
-      'is ',
-      'van',
-      'dal',
-      'ism ',
-      'and ',
-      'what ',
-      'isn',
-      "'",
-      't',
-      '.  ',
-      'I ',
-      'suggest ',
-      'you ',
-      'go ',
-      'do ',
-      'your ',
-      'put ',
-      'the ',
-      'computer ',
-      'down ',
-      'and ',
-      'do ',
-      'your ',
-      'homework',
-      '.  ',
-      'You ',
-      'can ',
-      'play ',
-      'on ',
-      'the ',
-      'weekend ',
-      'when ',
-      'school ',
-      'lets ',
-      'out',
-      '.'
-    ];
-
-    const shapValues = [
-      0.01092504, 0.00455374, -0.00485153, -0.00448904, -0.00224217,
-      -0.04614502, -0.00383218, -0.00510751, -0.06787664, -0.00415155,
-      -0.00535764, -0.00535764, -0.00371679, -0.00371679, -0.00326535,
-      -0.00326535, -0.00260804, -0.00260804, -0.00618124, -0.00205904,
-      -0.03055499, -0.01819205, -0.01296856, -0.0064224, -0.01313715,
-      -0.01091389, -0.00870228, -0.00464682, -0.00877311, -0.00895331,
-      -0.00924679, 0.00626233, 0.00452801, -0.00864476, -0.00701219,
-      -0.02690166, -0.01169196, -0.04226858, -0.07019774, 0.01310815,
-      -0.01112492, -0.00257022, 0.00244315, 0.00703637, 0.01107377, 0.0118604,
-      0.00137123, 0.0015346, -0.00076053, -0.00076053, 0.00096916, -0.00264306,
-      -0.04983622, -0.04860111, -0.00971938
-    ];
-
-    // Need to get the min and max of shap values across all classes
-    const shapRange: [number, number] = [Infinity, -Infinity];
-    for (const value of shapValues) {
-      if (value < shapRange[0]) {
-        shapRange[0] = value;
-      }
-      if (value > shapRange[1]) {
-        shapRange[1] = value;
-      }
-    }
-
-    // Make the shap range symmetric around 0
-    if (Math.abs(shapRange[1]) > Math.abs(shapRange[0])) {
-      if (shapRange[1] > 0) {
-        shapRange[0] = -shapRange[1];
-      } else {
-        shapRange[0] = shapRange[1];
-        shapRange[1] = -shapRange[1];
-      }
-    } else {
-      if (shapRange[0] < 0) {
-        shapRange[1] = -shapRange[0];
-      } else {
-        shapRange[1] = shapRange[0];
-        shapRange[0] = -shapRange[0];
-      }
-    }
-
-    // Update the shap scales
-    this.shapScale.domain(shapRange);
-    this.shapLengthScale.domain(shapRange);
-
-    const axisGroup = this.colorScaleSVG.select<SVGGElement>('g.axis-group');
-    this.colorLegendAxis = d3
-      .axisLeft(this.shapLengthScale)
-      .tickValues([
-        this.shapLengthScale.domain()[0],
-        0,
-        this.shapLengthScale.domain()[1]
-      ])
-      .tickFormat(d3.format('.2f'));
-    axisGroup.call(this.colorLegendAxis);
-
-    // Add the words to the text block
-    const textBlock = d3
-      .select(this.component)
-      .select<HTMLElement>('.text-block');
-    textBlock.selectAll('*').remove();
-
-    for (const [i, word] of words.entries()) {
-      const backColorStr = this.colorScale(this.shapScale(-shapValues[i]));
-      const backColor = d3.color(backColorStr)!.rgb();
-      const frontColor = d3.color(config.colors['gray-900'])!.rgb();
-
-      textBlock
-        .append('span')
-        .attr('class', 'text-word')
-        .text(word)
-        .style('background-color', backColorStr)
-        .classed(
-          'dark-background',
-          !haveContrast(
-            [backColor.r, backColor.g, backColor.b],
-            [frontColor.r, frontColor.g, frontColor.b]
-          )
-        );
-    }
-    console.log(words);
   };
 }
